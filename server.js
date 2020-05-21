3610f215c1c848faf32cf0bf62b1d4f07c7d0d2c
Daniel Ong <danielonges@Daniels-MacBook-Pro-2.local>
2020-05-21 13:05:23 +0800

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+    res.json({ msg: 'Wecome to the ContactKeeper API' })
+);
+
+// define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
